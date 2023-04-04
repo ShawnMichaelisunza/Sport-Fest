824df56258d0871af4c8aa38e2e6cd355efb5411
ShawnMichaelisunza <shawnisunza4@gmail.com>
2023-04-04 15:21:07 +0800

    Updating Files

--- news.php
+++ news.php
@@ -5,47 +5,53 @@
   <div class="news container-fluid" style="padding-top: 100px;">
   <div class="news-1 container">
   <h1>GAMES</h1>
+         <!-- Basketball -->
         <br>    
         <h4>Basketball</h4>
         <br>
-            <h1>Coming Soon....</h1>
+            <h1 class="h1-1">Coming Soon....</h1>
         <br>
+         <!-- Volleyball -->
         <h4>Volleyball</h4>
         <br>
 
-        <h1>Coming Soon....</h1>
+        <h1 class="h1-1">Coming Soon....</h1>
 
+        <!-- Badminton -->
         <br>
         <h4>Badminton</h4>
         <br>
         
-        <h1>Coming Soon....</h1>
+        <h1 class="h1-1">Coming Soon....</h1>
 
+        <!-- billiard -->
         <br>
         <h4>Billiard</h4>
         <br>
         
-        <h1>Coming Soon....</h1>
+        <h1 class="h1-1">Coming Soon....</h1>
 
+        <!-- Bowling -->
         <br>
         <h4>Bowling</h4>
         <br>
 
-        <h1>Coming Soon....</h1>
+        <h1 class="h1-1">Coming Soon....</h1>
 
-
+        <!-- Chess -->
         <br>
         <h4 style="color: white; text-shadow: 1px 1px 2px black;">Chess</h4>
         <br>
 
 
-        <h1 style="color: white; text-shadow: 1px 1px 2px red;">Coming Soon....</h1>
+        <h1 class="h1-1" style="color: white; text-shadow: 1px 1px 2px red;">Coming Soon....</h1>
 
 
-
+        <!-- Darts -->
         <br>
         <h4 style="color: white; text-shadow: 1px 1px 2px black;" >Darts</h4>
         <br>
+        <h4 style="color: red; text-align: center; font-family: 'arial'; font-weight: bolder;"><b>1st Round</b></h4>
         <div class="versus">
         <div class="score-1" style="border-top-right-radius: 20px; border-top-left-radius: 20px;">
         <div>
@@ -111,16 +117,19 @@
         </div>
         </div>
         <br>
+
+        <!-- Swimming -->
         <h4 style="color: white; text-shadow: 1px 1px 2px black;">Swimming</h4>
         <br>
     
-        <h1 style="color: white; text-shadow: 1px 1px 2px red;">Coming Soon....</h1>
-
+        <h1 class="h1-1" style="color: white; text-shadow: 1px 1px 2px red;">Coming Soon....</h1>
         <br>
+
+        <!-- zumba -->
         <h4 style="color: white; text-shadow: 1px 1px 2px black;">Zumba</h4>
         <br>
 
-        <h1 style="color: white; text-shadow: 1px 1px 2px red;">Coming Soon....</h1>
+        <h1 class="h1-1" style="color: white; text-shadow: 1px 1px 2px red;">Coming Soon....</h1>
 
 
         <br>
